65ffbab35b275f60676f0dab03eb489c36fc853d
DannyelYankee <daniel.juape@gmail.com>
2021-12-26 23:07:42 +0100

    docker-compose, recolector de metricas, leer bbdd funciona

---
+++ leer_bbdd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/ python3
+from influxdb import InfluxDBClient
+from datetime import datetime
+
+def leer_comentarios(client):
+    # Leer los comentarios existentes
+
+    result = client.query("SELECT * from metricas") # Query de todos los datos de todos los comentarios
+    comentarios = []    # Array que almacenará todos los comentarios leídos para guardarlos posteriormente
+    print("{:17}{:13}{:13}{:13}{:13}{:13}{:13}{:13}{:13}{}".format("Nombre","CPU","Mem. Usada","Mem. Limite","Memoria %", "Net imput", "Net Out", "Block In.", "Block Out.", "PIDs")) # Título. Para que quede elegante
+    for t in result:
+      for i in t:
+        # Loop por todos los comentarios leídos en nuestra query
+        print("{:14} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {}".format(i['nombre'],i['cpu'],i['mem_usada'],i['mem_limite'],i['mem_porcentaje'],i['net_input'],i['net_output'],i['block_input'],i['block_output'],i['pids']))
+
+
+def main_loop():
+    # Loop principal del programa
+
+    # Creamos el cliente InfluxDB con la librería InfluxDBClient, aportándole los datos necesarios.
+    client = InfluxDBClient(host = 'localhost', port = 8083, username = 'admin', password = 'admin', database = 'influx_db')
+    client.switch_database('influx_db') # equivalente a "USE influx_db"
+    leer_comentarios(client)
+
+
+if __name__ == "__main__":
+    main_loop()
---
+++ recolector.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+from influxdb import InfluxDBClient
+from datetime import datetime
+import subprocess
+import re
+def recolectar():
+    datos=subprocess.Popen(["sudo","docker","stats","--no-stream"],stdout=subprocess.PIPE)
+    output,err=datos.communicate()
+
+    metricas = output.decode("utf-8").split("\n")
+    #metricas = output_splitted[1].split("  ")
+    lista_dic = []
+    i = 1
+    while i < len(metricas)-1:
+        datos_splitteados = metricas[i].split(' ')
+        datos_limpios = limpiar_array(datos_splitteados)
+        dic = {
+            "nombre": datos_limpios[0],
+            "cpu": datos_limpios[1],
+            "mem_usada": datos_limpios[2],
+            "mem_limite": datos_limpios[3],
+            "mem_porcentaje": datos_limpios[4],
+            "net_input": datos_limpios[5],
+            "net_output": datos_limpios[6],
+            "block_input": datos_limpios[7],
+            "block_output": datos_limpios[8],
+            "pids": datos_limpios[9]
+        }
+        lista_dic.append(dic)
+        i+=1
+
+    return lista_dic
+
+def limpiar_array(array):
+    array_limpio = []
+    i = 0
+    while i <= len(array)-1:
+        if array[i] != '':
+            if i != 3:
+                array_limpio.append(re.findall(r'-?\d+\.?\d*',array[i]))
+            else:
+                array_limpio.append(array[i])
+        i+=1
+    array_limpio2 = []
+    i = 2
+    array_limpio2.append(array_limpio[1])
+    while i <= len(array_limpio)-1:
+        if array_limpio[i]:
+            array_limpio2.append(array_limpio[i][0])
+        i+=1
+    return array_limpio2
+
+def subir_metricas(client, metricas):
+    # Crear y escribir en la base de datos una nueva entrada
+    data = [] # Lista con todas las entradas que se vayan a subir a la BBDD
+    for entrada in metricas:
+      datos_actuales = {
+          "measurement": "metricas",      # - Algo similar a la "tabla" en la que almacenamos nuestros comentarios
+          "time": datetime.now(),         # - Campo que guarda la fecha y hora en la que es añadido un comentario
+          "fields": entrada.copy()        # - Los valores importantes que queremos almacenar se guardan en el diccionario
+      }
+      data.append(datos_actuales) # Añade la entrada actual a la lista de datos a subir
+
+    client.write_points(data)   # Escribe los datos en la BBDD
+
+
+
+def main():
+
+    # Creamos el cliente InfluxDB con la librería InfluxDBClient, aportándole los datos necesarios.
+    client = InfluxDBClient(host = 'localhost', port = 8083, username = 'admin', password = 'admin', database = 'influx_db')
+    client.switch_database('influx_db') # equivalente a "USE influx_db"
+    scrapeo = recolectar()
+    subir_metricas(client, scrapeo)
+
+if __name__ == "__main__":
+    main()
