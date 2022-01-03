4144eb457f71fadcd72172c45cf8b05e8de6467b
DannyelYankee <daniel.juape@gmail.com>
2022-01-03 17:25:14 +0100

    recolector con todas las metricas en bytes

--- recolector_metricas.py
+++ recolector_metricas.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from influxdb import InfluxDBClient
 from datetime import datetime
+import enum
 import subprocess
 import re
 def recolectar():
@@ -13,7 +14,9 @@ def recolectar():
     i = 1
 
     while i < len(metricas)-1:
+
         datos_splitteados = metricas[i].split(' ')
+        print (metricas[i])
         datos_limpios = limpiar_array(datos_splitteados)
         dic = {
             "nombre": datos_limpios[0],
@@ -34,23 +37,41 @@ def recolectar():
 
 def limpiar_array(array):
     array_limpio = []
-    i = 0
+    i = 1    
     while i <= len(array)-1:
         if array[i] != '':
             if i != 3:
-                array_limpio.append(re.findall(r'-?\d+\.?\d*',array[i]))
+                if 'MiB' in array[i] or 'GiB' in array[i] or 'MB' in array[i] or 'kB' in array[i]:
+                    numero,unidad=separar(array[i])                    
+                    numero_convertido = conversion(numero,unidad)
+                    array_limpio.append(numero_convertido)
+                else:
+                    dato = re.findall(r'-?\d+\.?\d*',array[i])
+                    if dato:
+                        array_limpio.append(dato.pop())                    
             else:
                 array_limpio.append(array[i])
         i+=1
-    array_limpio2 = []
-    i = 2
-    array_limpio2.append(array_limpio[1])
-    while i <= len(array_limpio)-1:
-        if array_limpio[i]:
-            array_limpio2.append(array_limpio[i][0])
-        i+=1
-    return array_limpio2
+    return array_limpio
 
+def conversion(numero, unidad):
+    if unidad == 'MiB':
+        return float(numero)*(1.049*(10**6))
+    elif unidad =='GiB':
+        return float(numero)*(1.074*(10**9))
+    elif unidad =='MB':
+        return float(numero)*1000000
+    elif unidad =='kB':
+        return float(numero)*1000
+
+def separar(string):
+    #dado un numero con su respectiva unidad lo separa; ejemplo: 11.63MiB --> (11.63,MiB)
+    for i,c in enumerate(string):
+        if not c.isdigit() and c != '.':
+            break
+    num = string[:i]
+    unit = string[i:]
+    return (num,unit)
 def subir_metricas(client, metricas):
     # Crear y escribir en la base de datos una nueva entrada
     data = [] # Lista con todas las entradas que se vayan a subir a la BBDD
